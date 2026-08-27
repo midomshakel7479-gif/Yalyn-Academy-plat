96deb3fcee4f040abc0ee2b4579fff0be8a1fad3
midomshakel7479-gif <mido.mshakel7479@gmail.com>
2026-08-27 12:13:57 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.YALYN_CONFIG = {
+  webAppUrl: "https://script.google.com/macros/s/AKfycbx2-jzindP82ZUGP7zhtDaffdTtqA3nCcPHqicGiFxxY1dTZKb6Tj6s02yK98PraxE8NA/exec",
+  geminiApiKey: "AQ.Ab8RN6L4HxZF4H88nCgRMKun67WErbjN1zwzavi9vvaYzzruVg"
+};
