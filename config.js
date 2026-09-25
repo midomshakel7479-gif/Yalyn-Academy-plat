50e910f831859e61215623218633127a0d9d4d17
midomshakel7479-gif <mido.mshakel7479@gmail.com>
2026-09-26 00:23:55 +0300

    config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.YALYN_CONFIG = {
   webAppUrl: "https://script.google.com/macros/s/AKfycbx2-jzindP82ZUGP7zhtDaffdTtqA3nCcPHqicGiFxxY1dTZKb6Tj6s02yK98PraxE8NA/exec",
-  geminiApiKey: "AQ.Ab8RN6L4HxZF4H88nCgRMKun67WErbjN1zwzavi9vvaYzzruVg"
+  geminiApiKey: "AQ.Ab8RN6Kc9t_4Jy0K7XchVO37VRHMCj1t5M81irB-70-5QN1fMw"
 };
